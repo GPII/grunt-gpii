c7001af728cf51c68f88d9b50325b8a08f9d9481
Antranig Basman <antranig.basman@colorado.edu>
2015-10-13 03:09:58 +0100

    GPII-1307: Corrected faulty implementation of shellImmediate as well as binding to new and correct "dedupe-infusion" micromodule

--- tasks/gpii.js
+++ tasks/gpii.js
@@ -2,6 +2,7 @@
 GPII Grunt shared tasks
 
 Copyright 2014 RTF-US
+Copyright 2015 RTF-I
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
@@ -12,8 +13,10 @@ https://github.com/gpii/universal/LICENSE.txt
 
 "use strict";
 
+var dedupeInfusion = require("dedupe-infusion");
+
 module.exports = function (grunt) {
-    var path = require("path"),
+    var shell = require("shelljs"),
         _ = grunt.util._; // for _.merge - we can't load Infusion since we are probably trying to resolve it during the build
  
         
@@ -41,57 +44,15 @@ module.exports = function (grunt) {
             }
         }
     };
-    
-    // Subvert the grunt invocation system by accepting a complex argument and stuffing it into the config's options for the task
-    // We can't invoke task functions directly since grunt queues them async
-    
-    gpiiGrunt.runTask = function (type, options) {
-        var holder = grunt.config.get(type) || {};
-        options = _.merge({}, gpiiGrunt.defaults[type], options);
-        if (options.name) {
-            holder[options.name] = options;
-        } else {
-            holder = options;
-        }
-        grunt.config.set(type, holder);
-        grunt.task.run(type + (options.name ? ":" + options.name : ""));
-    };
-    
+        
     grunt.registerTask("dedupe-infusion", "Remove duplicate copies of infusion", function () {
         var options = this.options(gpiiGrunt.defaults["dedupe-infusion"].options);
-        var node_modules = options.node_modules;
-        var infusions = grunt.file.expand({
-            cwd: node_modules
-        }, "**/infusion");
-        grunt.log.ok("Found " + infusions.length + " infusions");
-        var infusionSegs = infusions.map(function (onePath) {
-            // Whilst the "path" module exports an entry "path.sep" purportedly holding this path separator,
-            // in practice this is not the one used by the grunt file expander
-            return onePath.split("/");
-        });
-        // Locate the copy of infusion at the shortest path depth
-        infusionSegs.sort(function (a, b) {
-            return a.length - b.length;
-        });
-        infusions = infusionSegs.map(function (oneSegs) {
-            return oneSegs.join("/");
-        });
-        if (infusionSegs.length === 0) {
-            grunt.log.error("Warning - no instances of Infusion library discovered in " + path.resolve(node_modules));
-        }
-        if (infusionSegs.length > 2 && infusionSegs[0] === infusionSegs[1]) {
-            grunt.log.error("Warning - found two instances of Infusion at the same path depth: " + infusions[0] + " and " + infusions[1] + ": deleting the second arbitrarily");
-        }
-        for (var i = 1; i < infusions.length; i++) {
-            var toDelete = node_modules + "/" + infusions[i];
-            grunt.log.ok("Deleting " + toDelete);
-            grunt.file["delete"](toDelete, { force: true });
-        }
+        dedupeInfusion(options);
     });
     
     gpiiGrunt.shellImmediate = function (options) {
-        if (!options.name || !options.command) {
-            grunt.log.error("shellImmediate task must have options \"name\" and \"command\"");
+        if (!options.command) {
+            grunt.log.error("shellImmediate task must have option \"command\"");
         }
         options = _.merge({ // Oh for a Model Transformations framework
             options: {
@@ -100,14 +61,20 @@ module.exports = function (grunt) {
                 }
             }
         }, options);
-        gpiiGrunt.runTask("shell", options);
+        if (options.cwd) {
+            shell.pushd(options.cwd);
+        }
+        grunt.log.ok("grunt-gpii executing command " + options.command);
+        shell.exec(options.command);
+        if (options.cwd) {
+            shell.popd();
+        }
     };
     
     gpiiGrunt.gitClone = function (options) {
         var gitOptions = options.fastClone ? " --depth=1" : "";
         var gitCommand = "git clone " + gitOptions + " " + options.repoURL + " " + options.localPath;
         gpiiGrunt.shellImmediate({
-            name: "git-clone",
             command: gitCommand
         });
     };
@@ -122,7 +89,6 @@ module.exports = function (grunt) {
             }
             var installArg = entry.indexOf("git://") === 0 ? entry : dep + "@\"" + entry + "\"";
             gpiiGrunt.shellImmediate({
-                name: "npm-install",
                 command: "npm install " + installArg
             });
         }
@@ -140,19 +106,16 @@ module.exports = function (grunt) {
         });
 
         gpiiGrunt.shellImmediate({
-            name: "npm-install",
             command: "npm install",
             cwd: options.universal
         });
         
-        gpiiGrunt.runTask("dedupe-infusion", {
-            options: {
-                node_modules: "../node_modules/universal/node_modules"
-            }
+        dedupeInfusion({
+            node_modules: "../node_modules/universal/node_modules"
         });
     });
 
-    grunt.registerTask("lint", "Apply jshint and jsonlint", ["jshint", "jsonlint"]);
+    grunt.registerTask("lint", "Apply jshint", ["jshint"]);
     
     var packageFile = grunt.file.readJSON("package.json");
     
